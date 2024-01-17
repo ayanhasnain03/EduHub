ee1bb3d4cb5c4916013c7d92f37404704ab394d8
Ayan <ayan2572006@gmail.com>
2024-01-17 13:29:43 +0530

    done reset password ui

--- coursebundler/src/App.js
+++ coursebundler/src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Layout/Footer/Footer'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import ForgetPassword from './components/Auth/ForgetPassword'
+import ResetPassword from './components/Auth/ResetPassword'
 const App = () => {
   return (
     <Router>
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/forgetpassword' element={<ForgetPassword/>}/>
+        <Route path='/resetpassword/:token' element={<ResetPassword/>}/>
       </Routes>
       <Footer/>
     </Router>
