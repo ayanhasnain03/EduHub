c87a0b7fdca20e91c658c0b4dc8bd90786b594cd
Ayan <ayan2572006@gmail.com>
2024-01-21 18:19:14 +0530

    payment fail ui done

--- coursebundler/src/components/Payments/PaymentFail.jsx
+++ coursebundler/src/components/Payments/PaymentFail.jsx
@@ -1,8 +1,19 @@
+import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { RiCheckboxCircleFill, RiErrorWarningFill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 const PaymentFail = () => {
   return (
-    <div>PaymentFail</div>
+    <Container h="90vh">
+    <VStack justifyContent={'center'} h="full" spacing={'4'}>
+      <RiErrorWarningFill size={'5rem'} />
+      <Heading textTransform={"uppercase"} children={'Payment Fail'} />
+      <Link to="/subscribe">
+        <Button variant={'ghost'}>Try Again</Button>
+      </Link>
+    </VStack>
+  </Container>
   )
 }
 
