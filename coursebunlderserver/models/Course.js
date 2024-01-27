617f7b4d3e8af7bf6b7f5c41fce754ba2ed514fe
Ayan <ayan2572006@gmail.com>
2024-01-28 04:57:41 +0530

    done course model

--- coursebunlderserver/models/Course.js
+++ coursebunlderserver/models/Course.js
@@ -1,5 +1,71 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please enter course title"],
+    minLength: [4, "Title must be atleast 4 char"],
+    maxLength: [80, "Title can exceed 80 char"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter course description"],
+    minLength: [15, "Title must be atleast 15 char"],
+  },
+  lectures: [
+    {
+      video: {
+        title: {
+          type: String,
+          required: true,
+        },
+        description: {
+          type: String,
+          required: true,
+        },
+        video: {
+          public_id: {
+            type: String,
+            required: true,
+          },
+          url: {
+            type: String,
+            required: true,
+          },
+        },
+      },
+    },
+  ],
+  poster: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  numOfVideos: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+  createdBy: {
+    type: String,
+    required: [true, "Enter course creator name"],
+  },
+  createdAt: {
+    type: Date,
+    default: date.now,
+  },
+});
 
-export const Course= mongoose.model("Course",schema);
+export const Course = mongoose.model("Course", schema);
