770756b3628fb5b38dbecac1ffea6b64be99bf89
Ayan <ayan2572006@gmail.com>
2024-02-01 19:24:57 +0530

    Subscription cancel done

--- coursebunlderserver/controllers/paymentController.js
+++ coursebunlderserver/controllers/paymentController.js
@@ -65,9 +65,42 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
   );
 });
 
-export const getRazorPayKey = catchAsyncError(async(req,res,next)=>{
-    res.status(200).json({
-        success:true,
-        key:process.env.RAZORPAY_API_KEY,
-    })
-})
+export const getRazorPayKey = catchAsyncError(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    key: process.env.RAZORPAY_API_KEY,
+  });
+});
+
+export const cancelSubscription = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  const subscriptionId = user.subscription.id;
+
+  let refund = false;
+
+  await instance.subscriptions.cancel(subscriptionId);
+
+ const payment = await Payment.findOne({
+   razorpay_subscription_id: subscriptionId,
+ });
+
+ const gap = Date.now() - payment.createdAt;
+
+ const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
+
+  if (refundTime > gap) {
+   await instance.payments.refund(payment.razorpay_payment_id);
+    refund=true;
+ }
+await payment.remove();
+user.subscription.id=undefined;
+user.subscription.status=undefined;
+await user.save();
+  res.status(200).json({
+    success: true,
+    message: refund
+      ? "Subscription cancelled You will recieve full refund within 7 days"
+      : "Subscription cancelled Now Refund initiated as subscription was cancelled after 7 days.",
+  });
+});
--- coursebunlderserver/routes/paymentRoutes.js
+++ coursebunlderserver/routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticatedUser } from "../middlewares/auth.js";
-import { buySubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
+import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
 
 const router = express.Router();
 //Buy Subscription
@@ -11,4 +11,7 @@ router.route("/paymentverification").post(isAuthenticatedUser,paymentVerificatio
 
 //Get Payment Key
 router.route("/razorpaykey").get(getRazorPayKey)
+
+//Cancel Subscription
+router.route("/subscribe/cancel").delete(isAuthenticatedUser,cancelSubscription)
 export default router;
