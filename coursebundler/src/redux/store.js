7e6c6b338ecc3818ce99aecc184e8a15330a79dc
Ayan <ayan2572006@gmail.com>
2024-02-04 18:49:39 +0530

    redux setup

---
+++ coursebundler/src/redux/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit"
+const server = `https://edu-hub-1oxu.vercel.app/api/v1/`;
+const store = configureStore({
+    reducer:{
+        
+    }
+})
+export default store;
