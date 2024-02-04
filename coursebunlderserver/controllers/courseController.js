11a84788776ed231739ba81642c853cee590b974
Ayan <ayan2572006@gmail.com>
2024-02-04 15:00:04 +0530

    category and keyword & backend done

--- coursebunlderserver/controllers/courseController.js
+++ coursebunlderserver/controllers/courseController.js
@@ -5,7 +5,19 @@ import getDataUri from "../utils/dataUri.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import cloudinary from "cloudinary";
 export const getAllCourses = catchAsyncError(async (req, res, next) => {
-  const courses = await Course.find().select("-lectures");
+  const keyword = req.query.keyword || "";
+  const category = req.query.category || "";
+
+  const courses = await Course.find({
+    title: {
+      $regex: keyword,
+      $options: "i",
+    },
+    category: {
+      $regex: category,
+      $options: "i",
+    },
+  }).select("-lectures");
   res.status(200).json({
     success: true,
     courses,
@@ -94,9 +106,9 @@ export const deleteLecture = catchAsyncError(async (req, res, next) => {
   const { courseId, lectureId } = req.query;
   const course = await Course.findById(courseId);
   if (!course) return next(new ErrorHandler("Course not Found", 404));
-  const lecture = course.lectures = course.lectures.find((item) => {
+  const lecture = (course.lectures = course.lectures.find((item) => {
     if (item._id.toString() === lectureId.toString()) return item;
-  });
+  }));
   await cloudinary.v2.uploader.destroy(lecture.video.public_id, {
     resource_type: "video",
   });
@@ -112,14 +124,21 @@ export const deleteLecture = catchAsyncError(async (req, res, next) => {
   });
 });
 
-Course.watch().on("change",async()=>{
-  const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
-const courses = await Course.find({});
-totalViews =0;
-for (let i = 0; i < courses.length; i++) {
- totalViews+=courses[i].views;
-}
-stats[0].views=totalViews;
-stats[0].createdAt = new Date(Date.now());
-await stats[0].save();
-})
+Course.watch().on("change", async () => {
+  try {
+    const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
+    const courses = await Course.find({});
+
+    let totalViews = 0;
+
+    for (let i = 0; i < courses.length; i++) {
+      totalViews += courses[i].views;
+    }
+
+    stats[0].views = totalViews;
+    stats[0].createdAt = new Date(Date.now());
+    await stats[0].save();
+  } catch (error) {
+    console.error("Error updating stats:", error);
+  }
+});
