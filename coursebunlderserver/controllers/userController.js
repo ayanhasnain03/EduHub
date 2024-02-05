6f04e378f1bfa390a2685bc60670205ea868e757
Ayan <ayan2572006@gmail.com>
2024-02-05 22:29:41 +0530

    sd

--- coursebunlderserver/controllers/userController.js
+++ coursebunlderserver/controllers/userController.js
@@ -52,7 +52,6 @@ export const logout = catchAsyncError(async (req, res, next) => {
     .cookie("token", null, {
       expires: new Date(Date.now()),
       httpOnly: true,
-      httpOnly: true,
       secure: true,
       sameSite: "none",
     })
