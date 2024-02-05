453d90be98ebaff5c53fc9918d321c773a80d364
Ayan <ayan2572006@gmail.com>
2024-02-05 21:04:04 +0530

    i

--- coursebundler/src/redux/store.js
+++ coursebundler/src/redux/store.js
@@ -6,5 +6,5 @@ const store = configureStore({
     }
 })
 export default store;
-export const server = `http://localhost:4000/api/v1`;
+export const server = `https://edu-hub-1oxu.vercel.app/api/v1`;
 ////https://edu-hub-1oxu.vercel.app/api/v1
