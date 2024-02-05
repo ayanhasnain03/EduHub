8a61e4afc34bc5cab68df7f381072f7400bd7256
Ayan <ayan2572006@gmail.com>
2024-02-05 16:41:42 +0530

    fix logout bug

--- coursebunlderserver/controllers/userController.js
+++ coursebunlderserver/controllers/userController.js
@@ -52,6 +52,8 @@ export const logout = catchAsyncError(async (req, res, next) => {
     .cookie("token", null, {
       expires: new Date(Date.now()),
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
     })
     .json({
       success: true,
@@ -261,12 +263,11 @@ export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
     });
 });
 
-  User.watch().on("change", async () => {
-    const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
-    const subscription = await User.find({ "subscription.status": "active" });
-    stats[0].users = await User.countDocuments();
-    stats[0].subscription = subscription.length;
-    stats[0].createdAt = new Date(Date.now());
-    await stats[0].save();
-  });
-
+User.watch().on("change", async () => {
+  const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
+  const subscription = await User.find({ "subscription.status": "active" });
+  stats[0].users = await User.countDocuments();
+  stats[0].subscription = subscription.length;
+  stats[0].createdAt = new Date(Date.now());
+  await stats[0].save();
+});
